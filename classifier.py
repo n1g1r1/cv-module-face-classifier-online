1fa41d54dfa3b7814b932668b84330dbde489a4e
Christian Reichel <chrstn.rchl@gmail.com>
2018-03-11 19:21:11 +0100

    Add classifier.

---
+++ classifier.py
@@ -0,0 +1,18 @@
+# IMPORTS.
+import requests		# sending requests.
+import json 		# json data actions.
+import numpy as np  # checking data type.
+
+class NumPyArangeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist() # or map(int, obj)
+        return json.JSONEncoder.default(self, obj)
+
+def classify(server_url, image_np):
+
+    data = {}
+    data["image"]= json.dumps(image_np.tolist())
+    data["mode"]= "predict"
+    response = requests.post(server_url, json = data)
+    return response.json()["label"], float(response.json()["probability"])
